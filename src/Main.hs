51ae85bd60f52442505164838c28cab05c0ee468
Upadhyay <prayag.upd@gmail.com>
2018-11-22 18:54:01 -0800

    make var easy to understand

--- src/Main.hs
+++ src/Main.hs
@@ -24,13 +24,11 @@ import Data.Maybe
 import Data.String.Conversions
 import Data.Time.Calendar
 import GHC.Generics
---import Lucid
 import Network.HTTP.Media ((//), (/:))
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 import System.Directory
---import Text.Blaze
 import Text.Blaze.Html.Renderer.Utf8
 import qualified Data.Aeson.Parser
 import qualified Text.Blaze.Html
@@ -48,14 +46,14 @@ data User = User
 
 instance ToJSON User
 
-users1 :: [User]
-users1 =
+users :: [User]
+users =
   [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
   , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
   ]
 
-server1 :: Server UserAPI
-server1 = return users1
+userServer :: Server UserAPI
+userServer = return users
 
 userAPI :: Proxy UserAPI
 userAPI = Proxy
@@ -63,8 +61,8 @@ userAPI = Proxy
 -- 'serve' comes from servant and hands you a WAI Application,
 -- which you can think of as an "abstract" web application,
 -- not yet a webserver.
-app1 :: Application
-app1 = serve userAPI server1
+application :: Application
+application = serve userAPI userServer
 
 main :: IO ()
-main = run 8081 app1
+main = run 8081 application
